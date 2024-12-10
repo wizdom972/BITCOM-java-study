597033c68fcc0f7bef9eccb921f8a21e2d87e90c
wizdom972 <jihae0584@gmail.com>
2024-12-10 11:20:44 +0900

    ✨ SQL practice01

--- mariadb-practices/models/practice01.sql
+++ mariadb-practices/models/practice01.sql
@@ -16,37 +16,61 @@ order by hire_date asc;
 
 -- 문제3.
 -- 여직원과 남직원은 각 각 몇 명이나 있나요?(각각 쿼리 만들어서 각각 출력 또는 집계로 한 번에 해결)
+select gender, count(gender)
+from employees
+group by gender;
 
 
 -- 문제4.
 -- 현재(to_date='9999-01-01'), 근무하고 있는 직원 수는 몇 명입니까? (salaries 테이블을 사용) 
+select count(emp_no)
+from salaries
+where to_date='9999-01-01';
 
    
 -- 문제5.
 -- 부서는 총 몇 개가 있나요?
+select count(dept_no)
+from departments;
 
   
 -- 문제6.
 -- 현재 부서 매니저는 몇 명이나 있나요?(역임 매너저는 제외)
+select count(emp_no) from dept_manager;
 
  
 -- 문제7.
 -- 전체 부서를 출력하려고 합니다. 순서는 부서이름이 긴 순서대로 출력해 보세요.
-
+select dept_name
+from departments
+order by length(dept_name) desc;
 
 
 -- 문제8.
 -- 현재 급여가 120,000이상 받는 사원은 몇 명이나 있습니까?
+select count(emp_no)
+from salaries
+where salary >= 120000;
 
    
 -- 문제9.
 -- 어떤 직책들이 있나요? 중복 없이 이름이 긴 순서대로 출력해 보세요.
+select distinct title
+from titles
+order by length(title) desc;
 
 
 -- 문제10
 -- 현재 Engineer 직책의 사원은 총 몇 명입니까?
+select count(emp_no)
+from titles
+where title = "Engineer";
 
    
 -- 문제11
 -- 사번이 13250(Zeydy)인 직원의 직책 변경 상황을 시간순으로 출력해보세요.
+select *
+from titles
+where emp_no = "13250"
+order by from_date asc;
 
